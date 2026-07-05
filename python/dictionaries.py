2ddbcf6a81bb9ad72dfe0164a59d03757350755b
Chaarvi H Joshi <chaarvi.joshi2004@gmail.com>
2026-07-05 09:56:21 +0530

    implement dictionaries methods

---
+++ python/dictionaries.py
@@ -0,0 +1,25 @@
+# key value pairs
+
+student = {
+    "name": "chaarvi",
+    "age": 21,
+    "subjects" :{
+        "physics": 98,
+        "maths": 97,
+        "chem": 99
+    }
+}
+
+# print(student["subjects"]["maths"])
+# print(student.keys())
+# print(student.values())
+# student["city"]="udaipur"
+# print(student)
+# print(student.items())
+
+student2 = student.copy()
+print(student2)
+
+
+
+
