18ab1696f9ad6aa8ae0c6263a93ab6e61b7ce772
Chaarvi H Joshi <chaarvi.joshi2004@gmail.com>
2026-07-05 09:56:21 +0530

    add tuple methods and example

---
+++ python/tuple.py
@@ -0,0 +1,33 @@
+# tuples are immutable
+# num = ()
+# print(type(num))
+
+# stu={}
+# print(type(stu))
+
+# names=set()
+# print(type(names))
+
+
+numb=(1,2,3,2,3,4,2,1,2,3,3)
+# print(numb)
+# print(numb.count(2))
+# print(numb.index(3))
+# print(len(numb))
+# print(max(numb))
+# print(min(numb))
+# print(sum(numb))
+# print(numb[1:5])
+
+
+
+
+
+
+
+
+
+
+
+
+
