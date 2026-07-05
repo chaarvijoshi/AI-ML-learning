185a597e0e1daf4f3669fb20764aaf181f94375d
Chaarvi H Joshi <chaarvi.joshi2004@gmail.com>
2026-07-05 09:56:21 +0530

    add python basic file

---
+++ python/python-basic.py
@@ -0,0 +1,25 @@
+# name= input("enter your name: ")
+# roll = int(input("enter your roll number : "))
+
+
+# name="chaarvi joshi.  "
+# print(name.split()) # returns the list
+
+# name="kritvi is my friend"
+# print(name.strip())
+# print(name.isdigit())
+# print(name.replace("kritvi","manas"))
+# print(f"hello my name is {name}")
+# print(name.upper())
+# print(name.lower())
+# print(name.capitalize())
+
+# print(name.title())
+
+
+
+
+
+
+
+
