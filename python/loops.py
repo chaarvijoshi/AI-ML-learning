51a21026051bab2fdbd10942daf02a22499ebd5f
Chaarvi H Joshi <chaarvi.joshi2004@gmail.com>
2026-07-05 09:56:21 +0530

    implement loops using python

---
+++ python/loops.py
@@ -0,0 +1,132 @@
+# loops in python
+
+# for i in range(1,101):
+#     print(i)
+
+# for i in range(100,0,-1):
+#     print(i)
+
+# sum = 0
+# for i in range(1,10):
+#     sum=sum + i
+    
+# print(sum)    
+
+# num = int(input("enter the number n for range : "))
+# sum=0
+# for i in range(1,num+1):
+#     sum = sum + i
+
+# print(sum)    
+
+# num = int(input("enter the number : "))
+# for i in range(1,11):
+#     print(f"{num} * {i} = {num*i}")
+
+
+# sum = 0
+# for i in range(0,21,2):
+#     sum = sum + i
+
+# print(sum)
+
+# count the digits 
+# num = int(input("enter the number : "))
+# count = 0
+# while num>0:
+#     num1 = num % 10
+#     num = num // 10
+#     count=count+1
+
+# print(count)       
+
+
+# sum of digits
+# num = int(input("enter the number :"))
+# sum =0
+# while num>0:
+#     r = num % 10
+#     num = num // 10
+#     sum = sum + r
+
+# print(sum)    
+    
+# reverse of digits 
+    
+# num = int(input("enter the number : "))
+# while num>0:
+#     r = num % 10
+#     print(r)
+#     num = num // 10    
+
+# palindrome
+
+# num = int(input("enter the number : "))
+# save = num
+# sum = 0
+# while num>0:
+#     r = num % 10
+#     num = num // 10
+#     sum = sum*10 + r
+
+# if sum==save:
+#     print("palindrome")
+# else:
+#     print("not palindrome")         
+
+
+# armstrong
+
+# num=int(input("enter the number : "))
+# save=num
+# sum =0
+# while num>0:
+#     r=num%10
+#     num=num//10
+#     sum=sum+r*r*r
+    
+# if sum==save:
+#     print("armstrong")
+# else:
+#     print("not armstrong")        
+
+# factorial
+
+# n = int(input("enter the number : "))
+# fact = 1
+# for i in range(1,n+1):
+#     fact = fact * i
+# print(fact)    
+    
+# n= int(input("enter the number : "))
+# fact = 1
+# while n>0:
+#     fact = fact*n
+#     n=n-1
+    
+# print(fact)    
+    
+    
+# fabonicci
+# n= int(input("enter the digit : "))
+# a = 0
+# b= 1
+# for i in range(n):
+#     print(a , end=" ")
+#     c= a+b
+#     a=b
+#     b=c
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
