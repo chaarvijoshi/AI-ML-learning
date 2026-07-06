5b5a38a05bd4ab085b02f17aee09e6e202ec296f
Chaarvi H Joshi <chaarvi.joshi2004@gmail.com>
2026-07-06 16:47:11 +0530

    Update list examples

--- python/list.py
+++ python/list.py
@@ -1,37 +1,37 @@
-# list=[10,"charvi",21.6,9.8,'c']
-# print(list)
+list=[10,"charvi",21.6,9.8,'c']
+print(list)
 
 list=[10,20,30,40,50]
-# list.append(70)
-# list.insert(4,60)
-# list.remove(20)
-# list.pop(2)
-# a = list.copy()
-# print(a)
-# list.clear()
-# list.reverse()
-# list.sort()
-# list.sort(reverse=True)
-# print(list.count(4))
-# print(list.index(50))
-# li=sorted(list)
-# print(li)
-# print(list[:3])
-# print(list[2:5])
-# print(list[::3])
+list.append(70)
+list.insert(4,60)
+list.remove(20)
+list.pop(2)
+a = list.copy()
+print(a)
+list.clear()
+list.reverse()
+list.sort()
+list.sort(reverse=True)
+print(list.count(4))
+print(list.index(50))
+li=sorted(list)
+print(li)
+print(list[:3])
+print(list[2:5])
+print(list[::3])
 print(list)
 print(list[0])
 name=[]
 
-# while(True):
-#     n = input("enter your name : ")
+while(True):
+    n = input("enter your name : ")
     
-#     if n == "exit":
-#         break
+    if n == "exit":
+        break
     
-#     name.append(n)
+    name.append(n)
     
-# print(name)    
+print(name)    
 
 
 
