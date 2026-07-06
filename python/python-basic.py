dceb7918283b77cb174091e0a497bc5e12c1a748
Chaarvi H Joshi <chaarvi.joshi2004@gmail.com>
2026-07-06 16:50:22 +0530

    Update python-basic examples

--- python/python-basic.py
+++ python/python-basic.py
@@ -1,20 +1,20 @@
-# name= input("enter your name: ")
-# roll = int(input("enter your roll number : "))
+name= input("enter your name: ")
+roll = int(input("enter your roll number : "))
 
 
-# name="chaarvi joshi.  "
-# print(name.split()) # returns the list
+name="chaarvi joshi.  "
+print(name.split()) # returns the list
 
-# name="kritvi is my friend"
-# print(name.strip())
-# print(name.isdigit())
-# print(name.replace("kritvi","manas"))
-# print(f"hello my name is {name}")
-# print(name.upper())
-# print(name.lower())
-# print(name.capitalize())
+name="kritvi is my friend"
+print(name.strip())
+print(name.isdigit())
+print(name.replace("kritvi","manas"))
+print(f"hello my name is {name}")
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
 
-# print(name.title())
+print(name.title())
 
 
 
