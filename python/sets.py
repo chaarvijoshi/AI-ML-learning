900842b5b4326b988c71e8737a7ba312ddaf62e6
Chaarvi H Joshi <chaarvi.joshi2004@gmail.com>
2026-07-06 16:51:39 +0530

    commit sets example in python

--- python/sets.py
+++ python/sets.py
@@ -1,28 +1,28 @@
 #  sets removes duplicates automatically
 # it generates in random order
 
-# numbers={10,20,30,20,30,50,40,50}
-# numbers.add(80)
-# numbers.remove(30)
-# numbers.pop()
-# print(max(numbers))
-# print(min(numbers))
-# print(sum(numbers))
-# print(len(numbers))
-# print(30 in numbers)
-# print(numbers)
+numbers={10,20,30,20,30,50,40,50}
+numbers.add(80)
+numbers.remove(30)
+numbers.pop()
+print(max(numbers))
+print(min(numbers))
+print(sum(numbers))
+print(len(numbers))
+print(30 in numbers)
+print(numbers)
 
 # to create an empty set
-# empty_set = set()
+empty_set = set()
 
-# digit ={1,2,3,4,2,3}
-# values = {2, 6 ,7,8,8}
-# r = digit.union(values)
-# r = digit | values
-# r= digit & values
-# r = digit - values
-# r = digit ^ values
-# print(r)
+digit ={1,2,3,4,2,3}
+values = {2, 6 ,7,8,8}
+r = digit.union(values)
+r = digit | values
+r= digit & values
+r = digit - values
+r = digit ^ values
+print(r)
 
 
 
