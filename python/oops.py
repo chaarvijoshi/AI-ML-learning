944a8d05dc4caa4ebfc6ca57bbb40c4d852dc2c0
Chaarvi H Joshi <chaarvi.joshi2004@gmail.com>
2026-07-14 18:50:52 +0530

    implementing oops concept in python

--- python/oops.py
+++ python/oops.py
@@ -1,39 +1,39 @@
-# class student:
-#     pass
+class student:
+    pass
 
-# student1 = student()
+student1 = student()
 
-# student1.name = "chaarvi"
-# student1.age =21
-# student1.course = "python"
+student1.name = "chaarvi"
+student1.age =21
+student1.course = "python"
 
-# class student:
-#     def __init__(self,name,age):
-#        self.name=name
-#        self.age=age
+class student:
+    def __init__(self,name,age):
+       self.name=name
+       self.age=age
        
-#     def show(self):
-#         print(self.name)
-#         print(self.age)   
+    def show(self):
+        print(self.name)
+        print(self.age)   
         
-# student1 = student("chaarvi",21)
-# student2 = student("manas",22)        
+student1 = student("chaarvi",21)
+student2 = student("manas",22)        
 
-# student1.show()
-# student2.show()
+student1.show()
+student2.show()
 
-# class student:
-#     school = "ABC"
+class student:
+    school = "ABC"
     
-# s1 = student()
-# s2 = student()
+s1 = student()
+s2 = student()
 
-# student.school = "MMPS"
+student.school = "MMPS"
 
-# s2.school = "DPS"
+s2.school = "DPS"
 
-# print(s1.school)
-# print(s2.school)    
+print(s1.school)
+print(s2.school)    
 
 class person:
     def __init__(self,name,age):
